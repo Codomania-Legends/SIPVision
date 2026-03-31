ee9085f54ea69d8e3ad87b05e88da803aabeb95d
vidhiagrawa <vidhiagrawal2205@gmail.com>
2026-03-31 17:14:25 +0530

    adding routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router'
+import Home from './Components/Home/Home'
+import Navbar from './Components/Navbar/Navbar'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/navbar" element={<Navbar />} />
+        </Routes>
+    </BrowserRouter>
 )
