e84b712f25db5680e0e440ee8d78b5947d780149
AnshulVishwa <anshulvishwa2205@gmail.com>
2026-03-31 21:17:44 +0530

    Changing the form Format

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import DetailsOverview from '../DetailsOverview/DetailsOverview'
 import Charts from '../../Utilities/Charts'
+import { Link } from 'react-router'
 
 function Home() {
   return (
-    <div className='h-screen w-screen flex justify-center items-center'>
-        <DetailsOverview />
+    <div className='h-screen w-screen flex flex-col justify-evenly items-center pt-10'>
+      <Charts />
+      
     </div>
   )
 }
--- src/Components/UserDetails/UserDetails.jsx
+++ src/Components/UserDetails/UserDetails.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import DetailsOverview from '../DetailsOverview/DetailsOverview'
 
 function UserDetails() {
   return (
-    <div className='h-screen w-screen flex justify-center items-center'>UserDetails</div>
+    <div className='h-screen w-screen flex justify-center items-center'>
+      <DetailsOverview />
+    </div>
   )
 }
 
